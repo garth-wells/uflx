684a05359671b618f9a54e5e5abb0d77151c4800
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-02-17 09:24:01 +0000

    add finite element test

---
+++ test/test_finite_element.py
@@ -0,0 +1,21 @@
+"""Test cell."""
+
+import pytest
+from utils import point, interval, triangle, tetrahedron, LagrangeElement
+
+
+@pytest.mark.parametrize(
+    "cell",
+    [
+        point,
+        interval,
+        triangle,
+        tetrahedron,
+    ],
+)
+def test_lagrange_element(cell):
+    element = LagrangeElement(cell, 2)
+
+    assert element.cell == cell
+    assert element.reference_value_shape == ()
+    assert element.embedded_lagrange_superdegree == 2
