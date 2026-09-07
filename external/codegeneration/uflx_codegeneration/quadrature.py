2b351561df49ac3cf2fbb1f8ea1177e21720e41a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-09-07 15:24:11 +0100

    move integrals_to_quadrature function into quadrature.py

--- external/codegeneration/uflx_codegeneration/quadrature.py
+++ external/codegeneration/uflx_codegeneration/quadrature.py
@@ -5,11 +5,28 @@ from typing import Any
 
 import numpy as np
 import numpy.typing as npt
+from uflx.algorithms import replace
+from uflx.basis_functions import EvaluatedPhysicalBasisFunction, EvaluatedReferenceBasisFunction
+from uflx.domains import AbstractCoordinateElement, AbstractDomain
 from uflx.expressions import AbstractExpression
-from uflx.graphs import GraphNode
-from uflx.points import AbstractPoint, AbstractSetOfPoints
+from uflx.function_spaces import AbstractReferenceMappedFunctionSpace
+from uflx.functions import AbstractPhysicalFunction, Argument, ReferenceArgument
+from uflx.geometry import (
+    Jacobian,
+    JacobianDeterminant,
+    JacobianInverse,
+    JacobianInverseTranspose,
+    JacobianTranspose,
+    ReferenceToPhysical,
+    SingleSpatialCoordinate,
+)
+from uflx.graphs import Graph, GraphNode, as_graph
+from uflx.integrals import AbstractIntegral, AbstractMeasure, Measure
+from uflx.points import AbstractPoint, AbstractSetOfPoints, PointComponent
 
+from uflx_codegeneration import symbols
 from uflx_codegeneration.c import GenerateC
+from uflx_codegeneration.nodes import AddToLocalTensor, Loop
 from uflx_codegeneration.utils import indented
 
 
@@ -148,3 +165,130 @@ def quadrature_rule(
 ) -> QuadratureRule:
     """Create a quadrature rule."""
     return QuadratureRule(np.array(points), np.array(weights))
+
+
+def extract_domain(graph: Graph, node: GraphNode) -> AbstractDomain:
+    """Extract the domain associated with a node."""
+    domain: AbstractDomain | None = None
+    for i in graph.descendants(node):
+        if isinstance(i, AbstractPhysicalFunction):
+            if domain is None:
+                domain = i.function_space.domain
+            else:
+                assert domain == i.function_space.domain
+        if hasattr(i, "domain"):
+            if domain is None:
+                domain = i.domain
+            else:
+                assert domain == i.domain
+    assert domain is not None
+    return domain
+
+
+def integrals_to_quadrature(
+    expression: GraphNode,
+    rules: dict[AbstractMeasure, QuadratureRule],
+    variable_namer=symbols.global_variable_namer,
+) -> GraphNode:
+    """Replace integrals with quadrature."""
+    updated_nodes: dict[GraphNode, GraphNode] = {}
+    to_replace: dict[GraphNode, GraphNode] = {}
+
+    graph = as_graph(expression)
+    for node in graph.ordered_nodes():
+        if isinstance(node, AbstractIntegral):
+            rule = rules[node.measure]
+            qvariable = variable_namer.variable()
+            qpoint = QuadraturePoint(rule, qvariable)
+
+            tensor_shape_components = {}
+
+            if not isinstance(node.measure, Measure):
+                raise NotImplementedError()
+            if node.measure._codim != 0 or node.measure._boundary_only:
+                raise NotImplementedError("Only codim 0 integrals supported for now")
+
+            arguments = []
+            for i in graph.descendants(node):
+                if isinstance(i, (Argument, ReferenceArgument)) and i.integral_label == node.label:
+                    arguments.append(i)
+                if isinstance(i, SingleSpatialCoordinate):
+                    domain = extract_domain(graph, node)
+                    if not isinstance(domain, AbstractCoordinateElement):
+                        raise NotImplementedError(
+                            "Code generation only implemented for reference mapped domain"
+                        )
+                    to_replace[i] = PointComponent(
+                        ReferenceToPhysical(qpoint, domain), i._component
+                    )
+                if isinstance(i, Jacobian) and i.point is None:
+                    to_replace[i] = Jacobian(i.domain, qpoint)
+                if isinstance(i, JacobianInverse) and i.point is None:
+                    to_replace[i] = JacobianInverse(i.domain, qpoint)
+                if isinstance(i, JacobianTranspose) and i.point is None:
+                    to_replace[i] = JacobianTranspose(i.domain, qpoint)
+                if isinstance(i, JacobianInverseTranspose) and i.point is None:
+                    to_replace[i] = JacobianInverseTranspose(i.domain, qpoint)
+                if isinstance(i, JacobianDeterminant) and i.point is None:
+                    to_replace[i] = JacobianDeterminant(i.domain, qpoint)
+            for i in arguments:
+                i_space = i.function_space
+                if not isinstance(i_space, AbstractReferenceMappedFunctionSpace):
+                    raise NotImplementedError(
+                        "Code generation only implemented for reference mapped spaces"
+                    )
+                if len(i_space.elements) != 1:
+                    raise NotImplementedError(
+                        "Code generation currently only implemented for spaces with "
+                        "exactly one element"
+                    )
+                tensor_shape_components[i.component_index] = i_space.elements[0].dim
+            tensor_shape = tuple(
+                tensor_shape_components.get(i, 1)
+                for i in range(
+                    min(tensor_shape_components.keys()), max(tensor_shape_components.keys()) + 1
+                )
+            )
+            variables = tuple(
+                "0" if component == 1 else variable_namer.variable() for component in tensor_shape
+            )
+
+            for a in arguments:
+                assert isinstance(a.function_space, AbstractReferenceMappedFunctionSpace)
+                assert isinstance(a.function_space.domain, AbstractCoordinateElement)
+                if isinstance(a, Argument):
+                    to_replace[a] = EvaluatedPhysicalBasisFunction(
+                        a.function_space,
+                        a.function_space.elements[0],
+                        variables[a.component_index],
+                        ReferenceToPhysical(qpoint, a.function_space.domain),
+                    )
+                elif isinstance(a, ReferenceArgument):
+                    to_replace[a] = EvaluatedReferenceBasisFunction(
+                        a.function_space.elements[0],
+                        variables[a.component_index],
+                        qpoint,
+                    )
+
+            domain = arguments[0].function_space.domain
+            for a in arguments:
+                assert a.function_space.domain == domain
+
+            assert isinstance(domain, AbstractCoordinateElement)
+            integrand = QuadratureWeight(rules[node.measure], qvariable) * node.integrand
+
+            body = AddToLocalTensor(variables, tensor_shape, integrand)
+
+            qloop = QuadratureLoop(body, rules[node.measure], qvariable)
+
+            next: GraphNode = qloop
+            for variable, count in zip(variables[::-1], tensor_shape[::-1]):
+                if variable == "0":
+                    continue
+                assert isinstance(count, int)
+                loop = Loop(variable, 0, count, next)
+                next = loop
+
+            updated_nodes[node] = next
+
+    return replace(updated_nodes.get(graph.root, graph.root), to_replace)
