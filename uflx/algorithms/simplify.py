b8f331692c07d79f710076cd1ffd44afac136600
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-09-16 16:24:27 +0100

    add simplify algorithm structure

---
+++ uflx/algorithms/simplify.py
@@ -0,0 +1,8 @@
+"""Simplifying expressions."""
+
+from uflx.expressions import AbstractExpression
+
+
+def simplify(form: AbstractExpression) -> AbstractExpression:
+    """Apply simplifications to an expression."""
+    return form
